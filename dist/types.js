a49345a1438a68c113269293970e09e0475abf79
PresidentTree94 <karlysams1218@gmail.com>
2026-03-10 17:58:07 -0400

    Edited types.

--- dist/types.js
+++ dist/types.js
@@ -1,7 +1,1 @@
 export {};
-/*
-export type FormElement<Value, Option = Value> = FieldConfig<Value, Option> & {
-  value: Value;
-  setValue: (value: string | string[]) => void;
-};
-*/ 
--- src/types.ts
+++ src/types.ts
@@ -11,22 +11,7 @@ export type Schema<T extends object> = {
   [K in keyof T]: FieldConfig<T[K], T[K] extends (infer U)[] ? U : T[K]>;
 };
 
-/*
-export type Schema<T extends object> = {
-  [K in keyof T]: T[K] extends (infer U)[]
-    ? FieldConfig<T[K], U>
-    : FieldConfig<T[K]>;
-};
-*/
-
 export type FormElement<Value> = FieldConfig<Value, Value extends (infer Option)[] ? Option : Value> & {
-    value: Value;
-    setValue: (value: string | string[]) => void;
-};
-
-/*
-export type FormElement<Value, Option = Value> = FieldConfig<Value, Option> & {
   value: Value;
   setValue: (value: string | string[]) => void;
 };
-*/
